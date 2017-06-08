f2a6abbf324a16b24f3003c008d6d6c22bbf1411
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-08 13:01:34 -0700

    get rid of pure funcs

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,37 +1,3 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
-
-// Pure Function
-function add(a, b) {
-  return a + b;
-}
-
-var a = 3;
-function add(b) {
-  return a + b;
-}
-
-var result;
-function add (a, b) {
-  result = a+b;
-  return result;
-}
-
-function add(a, b) {
-  return a + b + new Date().getSeconds();
-}
-
-function changeProp(obj) {
-  return {
-    ...obj,
-    name: 'Jen'
-  };
-}
-
-var res = changeProp({
-  name: 'Kyle',
-  age: 25
-});
-
-console.log(res);
