1b4a6f24e4b3d20e6b151086fc7f4a06c305c2ae
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-08 16:05:31 -0700

    stopping at redux 111

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,13 +2,42 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
       };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies, {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'ADD_MOVIE':
+    return {
+      ...state,
+      movies: [
+        ...state.movies, {
+          id: nextMovieId++,
+          title: action.title,
+          genre: action.genre
+        }
+      ]
+    };
     default:
       return state;
   }
@@ -21,6 +50,8 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
   console.log('name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+  
+  console.log('new state', store.getState());
 });
 // unsubscribe();
 
@@ -36,8 +67,16 @@ var action = {
 store.dispatch(action);
 
 var action = {
-  type: 'CHANGE_NAME',
-  name: 'john'
+  type: 'ADD_HOBBY',
+  hobby: 'running'
+};
+
+store.dispatch(action);
+
+var action = {
+  type: 'ADD_MOVIE',
+  title: 'mad max',
+  genre: 'action'
 };
 
 store.dispatch(action);
