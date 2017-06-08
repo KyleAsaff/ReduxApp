95bd1ed55b547c4fbfa1b5ca42a67ccf5dac371e
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-08 13:00:52 -0700

    fix scss and create pure functions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,3 +1,37 @@
 var redux = require('redux');
 
 console.log('Starting redux example');
+
+// Pure Function
+function add(a, b) {
+  return a + b;
+}
+
+var a = 3;
+function add(b) {
+  return a + b;
+}
+
+var result;
+function add (a, b) {
+  result = a+b;
+  return result;
+}
+
+function add(a, b) {
+  return a + b + new Date().getSeconds();
+}
+
+function changeProp(obj) {
+  return {
+    ...obj,
+    name: 'Jen'
+  };
+}
+
+var res = changeProp({
+  name: 'Kyle',
+  age: 25
+});
+
+console.log(res);
